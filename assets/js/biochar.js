7a646314ffa98c5cd8868db537a459b23d208e36
biochar_admin <chartech.biochar@outlook.com>
2023-03-01 14:04:03 -0600

    Adding in image file, and updating code with css, js, and remix icons, added sections and more formatting

---
+++ assets/js/biochar.js
@@ -0,0 +1,13 @@
+
+/* When scrolling, add padding to top or bottom dependindg and remove certain padding */
+const navbar = document.querySelector('.navbar')
+window.addEventListener('scroll',()=>{
+if(this.scrollY > 0 ){
+    navbar.classList.remove('py-4')
+    navbar.classList.add('shadow','py-3')
+}else{
+    navbar.classList.add('py-4')
+    navbar.classList.remove('shadow','py-3')
+}
+
+})
